4108cc1f1c1abeddf943a50ef70f58046950cd8a
Luca Hagel <luca@luhagel.com>
2021-03-13 03:19:40 +0100

    Ops: Add release config

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :praxis, PraxisWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
