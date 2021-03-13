d0838de243ce220914fcebecb60f792b20be408c
Luca Hagel <luca@luhagel.com>
2021-03-13 16:50:44 +0100

    Tests: Decrease :log_rounds in testing

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Praxis.MixProject do
       {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false},
       {:sobelow, "~> 0.8", only: :dev},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
